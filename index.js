2fc80eeb0d597911ba69104f139dad81f86d24f2
Kirankumar Sripati <krnsripati@gmail.com>
2019-09-13 00:52:18 +0530

    3.15: phonebook database, step3

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id).then(returnedObject => {
+    response.status(204).end()
+  })
 })
 
 app.get('/info', (request, response) => {
